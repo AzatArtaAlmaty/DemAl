14bc2b7d293ada9c7e6dd9fb45c009dec1516825
Azat <azat.baxit@gmail.com>
2019-11-13 12:53:03 +0600

    some changes

--- TimeTable/urls.py
+++ TimeTable/urls.py
@@ -4,8 +4,8 @@ from .views import *
 
 urlpatterns = [
     path('', index, name='index'),
-    path('test/', EventView.as_view()),
-    path('order/', OrderView.as_view()),
+    path('test', EventView.as_view()),
+    path('order', OrderView.as_view()),
     # path('<int:event_id>/', views.detail, name='detail'),
     # path('AllElements/', views.all_element, name='all_element'),
     # path('/<str:field_name>', views.all_element, name='all_element'),
